9dc7cbab85e3b7f1b7500d7a60b2d3f103e9c51a
Tiago Mesquita <tiagomac@gmail.com>
2019-08-05 11:12:04 -0300

    recuperando cliente

---
+++ src/main/java/github/tiagomac/cadastrocliente/Exemplo.java
@@ -0,0 +1,32 @@
+package github.tiagomac.cadastrocliente;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import github.tiagomac.cadastrocliente.model.Cliente;
+
+public class Exemplo {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Clientes-PU");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		// recuperar cliente
+//		Cliente cliente = entityManager.find(Cliente.class, 1);
+//		System.out.println(cliente);
+		
+		
+		Cliente cliente = new Cliente();
+		cliente.setId(2);
+		cliente.setNome("Autopeças Estrada");
+		
+//		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+//		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
--- src/main/java/github/tiagomac/cadastrocliente/model/Cliente.java
+++ src/main/java/github/tiagomac/cadastrocliente/model/Cliente.java
@@ -1,15 +1,19 @@
 package github.tiagomac.cadastrocliente.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "cliente")
 public class Cliente {
 
+	@Id
 	private Integer id;
+	
 	private String nome;
-	
-	
+
 }
